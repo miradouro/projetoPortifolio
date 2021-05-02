d913f1e6592d8e8c9dcf7dc153a3391732496cdb
Miradouro <rafaelmiradouro@gmail.com>
2021-05-02 09:01:29 -0300

    corrigindo configuração2

--- projetinho/settings.py
+++ projetinho/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['rmb-clientes.herokuapp.com/']
+ALLOWED_HOSTS = ['https://rmb-clientes.herokuapp.com/']
 
 
 # Application definition
