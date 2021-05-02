1ac0683a35e3eafcd9603c04483cccc09768f9d7
Miradouro <rafaelmiradouro@gmail.com>
2021-05-02 09:06:27 -0300

    corrigindo configuração3

--- projetinho/settings.py
+++ projetinho/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 from decouple import config
 from dj_database_url import parse as dburl
@@ -147,3 +148,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
